7079a048a91c47cde183339cde60727c4affce05
mpy-pbelucci <80411733+mpy-pbelucci@users.noreply.github.com>
2021-06-06 13:58:46 -0300

    Initial Commit

---
+++ memoryGame/memoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryGameApp.swift
+//  memoryGame
+//
+//  Created by Paulo Belucci on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct memoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
